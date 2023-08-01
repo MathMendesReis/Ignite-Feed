f1e5c45c239c67f954ae7305253c6e636de7f2b5
MathMendesReis <matheusmendesreis@outlook.com>
2023-08-01 13:55:00 -0300

    build

--- vite-project/src/pages/Main/componentsMain/Posts/FormNewComentInPost/index.tsx
+++ vite-project/src/pages/Main/componentsMain/Posts/FormNewComentInPost/index.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { Container } from "./styled";
 
 export default function InsertNewComentsInPost() {
+
+  // @ts-ignore
   const [comentsTextAreaValue,setComentsTextAreaValue] = useState('')
   const isDisabled = comentsTextAreaValue ===''? true:false
   return (
@@ -9,7 +11,9 @@ export default function InsertNewComentsInPost() {
       <textarea
       name=""
       id=""
+      // @ts-ignore
       cols="30"
+      // @ts-ignore
       rows="10"
       defaultValue={''}
       required
